f5ced8d2c2c7b776a9965f76eefc58fffc912572
ivanbodnar <ibodnar@outlook.com>
2019-05-29 17:20:36 -0300

    Tests for TestCsvFetcher added

--- bici_estaciones/csv_manager/test_manager.py
+++ bici_estaciones/csv_manager/test_manager.py
@@ -1,8 +1,9 @@
 from collections import OrderedDict
+from datetime import time
 
 import pytest
 
-from bici_estaciones.csv_manager.manager import CsvConnector, Row
+from bici_estaciones.csv_manager.manager import CsvConnector, Row, CsvFetcher
 
 
 class TestCsvConnector:
@@ -31,3 +32,19 @@ class TestRow:
     def test_start_station(self):
         row = Row(' Station ')
         assert row.start_station == 'station'
+
+
+class TestCsvFetcher:
+
+    def test_creates(self):
+        connector = CsvConnector('./bici_estaciones/csv_manager/recorridos.csv')
+        fetcher = CsvFetcher(connector, '06:00:00', '11:59:00')
+        assert isinstance(fetcher._filter_range_start, time)
+        assert isinstance(fetcher._filter_range_end, time)
+
+    def test_is_in_time_range(self):
+        connector = CsvConnector('./bici_estaciones/csv_manager/recorridos.csv')
+        fetcher = CsvFetcher(connector, '06:00:00', '11:59:00')
+        assert fetcher._is_in_time_range('2018-01-01 09:31:00')
+        assert not fetcher._is_in_time_range('2018-01-01 05:59:00')
+        assert not fetcher._is_in_time_range('2018-01-01 12:00:00')
