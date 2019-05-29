b0c9645b723cb967690b930ddb7eda56501935d9
ivanbodnar <ibodnar@outlook.com>
2019-05-29 17:55:16 -0300

    --amend

--- bici_estaciones/main.py
+++ bici_estaciones/main.py
@@ -2,4 +2,4 @@ from bici_estaciones.csv_manager.manager import CsvConnector, CsvFetcher
 
 c = CsvConnector('./bici_estaciones/csv_manager/recorridos.csv')
 fetcher = CsvFetcher(c, '06:00:00', '11:59:00')
-print(fetcher.fetch_filtered())
+print(fetcher.fetch_filtered(3))
