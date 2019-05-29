2e7945a6bf9a50b0c1718783b462c03547c247fa
ivanbodnar <ibodnar@outlook.com>
2019-05-29 19:22:41 -0300

    fn run added to main.py

--- bici_estaciones/main.py
+++ bici_estaciones/main.py
@@ -1,7 +1,13 @@
 from bici_estaciones.csv_manager.manager import CsvConnector, CsvFetcher
 
-c = CsvConnector('./bici_estaciones/csv_manager/recorridos.csv')
-fetcher = CsvFetcher(c, '06:00:00', '11:59:00')
 
-for station in fetcher.fetch_filtered(3):
-    print(station)
+def run():
+    connector = CsvConnector('./bici_estaciones/csv_manager/recorridos.csv')
+    fetcher = CsvFetcher(connector, '06:00:00', '11:59:00')
+    print('Procesando los Datos...')
+    for station in fetcher.fetch_filtered(3):
+        print(station)
+
+
+if __name__ == '__main__':
+    run()
